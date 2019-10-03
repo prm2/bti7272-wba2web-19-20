5063e64972f7110f83a9dbbd076e4a00035d5b2f
Marcel Pfahrer <marcel.pfahrer@bfh.ch>
2019-10-03 23:44:37 +0200

    Ex01 core version

---
+++ 02 Controllers/Ex01core/Controllers/OrgUnitController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ex01core.Controllers
+{
+    [Route("OrgUnit/{action=show}/{*path=/}")]
+    public class OrgUnitController : Controller
+    {
+        public string Show(string path)
+        {
+            return $"show org unit '{path}'";
+        }
+
+        public string Edit(string path)
+        {
+            return $"edit org unit '{path}'";
+        }
+    }
+}
