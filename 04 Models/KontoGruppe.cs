83495cdfafc1f3e26baed735125f33ca52da4776
Marcel Pfahrer <marcel.pfahrer@bfh.ch>
2019-10-18 09:16:01 +0200

    04 Models => data classes and objects

---
+++ 04 Models/KontoGruppe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsExamples.Models
+{
+    public class KontoGruppe
+    {
+        public string Id { get; set; }
+        public int Level { get; set; }
+        public string Name { get; set; }
+        public string Beschreibung { get; set; }
+        public KontoGruppenListe Untergruppen { get; } = new KontoGruppenListe();
+    }
+
+    public class KontoGruppenListe : List<KontoGruppe>
+    {
+        public KontoGruppe Find(string id)
+        {
+            foreach (var g in this)
+            {
+                if (g.Id == id)
+                    return g;
+                else if (id.StartsWith(g.Id))
+                    return g.Untergruppen.Find(id);
+            }
+
+            return null;
+        }
+    }
+}
