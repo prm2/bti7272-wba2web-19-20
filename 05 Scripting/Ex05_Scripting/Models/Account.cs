6cd15ed544010dd0a547ed4fc3aeedf4debbec33
Marcel Pfahrer <marcel.pfahrer@bfh.ch>
2019-11-15 09:15:06 +0100

    scripting

---
+++ 05 Scripting/Ex05_Scripting/Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex05_Scripting.Models
+{
+    public partial class Account
+    {
+        public string Type { get; set; }
+        public string SubjectId { get; set; }
+        public string FunctionType { get; set; }
+        public string FunctionId { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+
+        public virtual AccountGroup AccountGroup { get; set; }
+        public virtual AccountGroup Function { get; set; }
+    }
+}
---
+++ 05 Scripting/Ex05_Scripting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ex05_Scripting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
