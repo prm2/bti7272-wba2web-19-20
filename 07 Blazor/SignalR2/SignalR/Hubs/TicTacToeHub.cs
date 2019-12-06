1238742ab3efc122b8e8d9f1afec3c453a7221e7
Marcel Pfahrer <marcel.pfahrer@bfh.ch>
2019-12-06 08:38:51 +0100

    SignalR example

---
+++ 07 Blazor/SignalR2/SignalR/Hubs/TicTacToeHub.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalR.Models;
+using System.Threading;
+
+namespace SignalR.Hubs
+{
+    public class TicTacToeHub : Hub
+    {
+        int y = (new Random()).Next(999);
+
+        // Called by a client to start or join a game.
+        // - id  Id of the game
+        public void Join(int id)
+        {
+            var x = y;
+
+            var game = (TicTacToeGame.Games.ContainsKey(id)) 
+                ? TicTacToeGame.Games[id] 
+                : null;
+
+            if ((game == null) || (game.Status == TicTacToeGame.GameStatus.Ended))
+            {   // start a new game and wait for opponent
+                game = new TicTacToeGame(id, Context.ConnectionId);
+                TicTacToeGame.Games[id] = game;
+                Clients.Caller.join(id, 'X', ' ');
+            }
+            else if (game.Status == TicTacToeGame.GameStatus.Initialized)
+            {   // join a game and prompt the waiting client
+                var ch = game.Join(Context.ConnectionId);
+                Clients.Caller.join(id, ch, game.Next);
+                Clients.Client(game.ConnectionX).prompt(id, game.Next);
+            }
+        }
+
+        // Called by a client when playing, i.e. setting a field.
+        // - id  Id of the game
+        // - x   X-Position of the field to set
+        // - y   Y-Position of the field to set
+        // - ch  Character to set
+        public void Play(int id, int x, int y, char ch)
+        {
+            var game = (TicTacToeGame.Games.ContainsKey(id))
+                ? TicTacToeGame.Games[id]
+                : null;
+
+            if (game != null)
+            {
+                game.SetField(x, y, ch);
+                Clients.All.setField(id, x, y, ch);
+                if (game.Status == TicTacToeGame.GameStatus.Running)
+                {
+                    Clients.Client(game.ConnectionO).prompt(id, game.Next);
+                    Clients.Client(game.ConnectionX).prompt(id, game.Next);
+                }
+                else
+                {
+                    Clients.Client(game.ConnectionO).ended(id, game.Winner);
+                    Clients.Client(game.ConnectionX).ended(id, game.Winner);
+                }
+            }
+        }
+    }
+}
---
+++ 07 Blazor/SignalR2/SignalR/Models/TicTacToeGame.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Models
+{
+    public class TicTacToeGame
+    {
+        char[,] fields;
+
+        public TicTacToeGame(int id, string connectionId)
+        {
+            this.Id = id;
+            fields = new char[,] { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+            this.Status = GameStatus.Initialized;
+            this.Next = 'X';
+            this.ConnectionX = connectionId;
+        }
+
+        public int Id { get; protected set; }
+        public string ConnectionX { get; protected set; }
+        public string ConnectionO { get; protected set; }
+
+        public enum GameStatus { Initialized, Running, Ended };
+
+        public GameStatus Status { get; protected set; }
+
+        public char Winner { get; protected set; }
+
+        public char Next { get; protected set; }
+
+        public char Join(string connectionId)
+        {
+            if (Status != GameStatus.Initialized)
+                return ' ';
+
+            Status = GameStatus.Running;
+            this.ConnectionO = connectionId;
+
+            return 'O';
+        }
+        
+        public void SetField(int x, int y, char ch)
+        {
+            if (Status != GameStatus.Running)
+                throw new Exception("invalid status");
+
+            if (ch != Next)
+                throw new Exception("Not your turn");
+
+            Next = (ch == 'O') ? 'X' : 'O';
+
+            if ((x < 0) || (x >= 3) || (y < 0) || (y >= 3))
+                throw new Exception("Invalid field");
+
+            if (fields[x, y] != ' ')
+                throw new Exception("Field is not empty");
+
+            fields[x, y] = ch;
+
+            if (((fields[0, 0] == ch) && (fields[1, 1] == ch) && (fields[2, 2] == ch)) ||
+                ((fields[0, 2] == ch) && (fields[1, 1] == ch) && (fields[2, 0] == ch)))
+            {
+                Status = GameStatus.Ended;
+                Winner = ch;
+            }
+            else
+                for (int i=0; i<3; i++)
+                    if (((fields[i, 0] == ch) && (fields[i, 1] == ch) && (fields[i, 2] == ch)) ||
+                        ((fields[0, i] == ch) && (fields[1, i] == ch) && (fields[2, i] == ch)))
+                    {
+                        Status = GameStatus.Ended;
+                        Winner = ch;
+                    }
+
+            if (Status != GameStatus.Ended)
+            {
+                var ended = true;
+                foreach (var f in fields)
+                    if (f == ' ')
+                        ended = false;
+
+                if (ended)
+                {
+                    Status = GameStatus.Ended;
+                    Winner = ' ';
+                }
+            }
+        }
+
+        public static Dictionary<int, TicTacToeGame> Games = new Dictionary<int, TicTacToeGame>();
+    }
+
+}
