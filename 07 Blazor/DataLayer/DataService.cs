880929070c5e1e33a6218731ced62a0eeec4d996
Marcel Pfahrer <marcel.pfahrer@bfh.ch>
2019-12-13 08:35:27 +0100

    DataLayer for Ex07

---
+++ 07 Blazor/DataLayer/DataService.cs
@@ -0,0 +1,40 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class DataService
+    {
+        public async Task<List<Account>> AccountsAsync(string id = null)
+        {
+            using (var db = new WBA2WebContext())
+            {
+                if (id == null)
+                    return await db.Account.ToListAsync();
+                else
+                    return await db.Account
+                        .Where(a => a.FunctionId == id)
+                        .ToListAsync();
+            }
+        }
+
+        public async Task<IEnumerable<AccountGroup>> ER()
+        {
+            using (var db = new WBA2WebContext())
+            {
+                return await db.AccountGroup
+                    .Include(a => a.SubGroups)
+                    .Include(a => a.FunctionAccounts)
+                    .Where(a => a.Level > 0 && a.Type == "FG")
+                    .OrderBy(a => a.Id)
+                    .ToListAsync();
+            }
+        }
+
+    }
+}
---
+++ 07 Blazor/DataLayer/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.Models
+{
+    public partial class Account
+    {
+        public string Type { get; set; }
+        public string SubjectId { get; set; }
+        public string FunctionType { get; set; }
+        public string FunctionId { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+
+        public virtual AccountGroup AccountGroup { get; set; }
+        public virtual AccountGroup Function { get; set; }
+
+        public string Id
+        {
+            get
+            {
+                return $"{FunctionId}.{SubjectId}.{Number:D2}";
+            }
+        }
+    }
+}
